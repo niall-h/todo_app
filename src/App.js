90235530d97dda86feaeba62ea3fafdf233db424
Nyan Min Htet <niallhtet@gmail.com>
2021-11-17 20:07:47 -0800

    Added some functionality and used local storage to restore state on refresh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import "./App.css";
 import { useState } from "react";
 
+const TODO_KEY = "todos";
+
+const getLocalStorage = () => {
+  const value = localStorage.getItem(TODO_KEY);
+  return JSON.parse(value);
+};
+
+const updateLocalStorage = (todos) => {
+  return localStorage.setItem(TODO_KEY, JSON.stringify(todos));
+};
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const previous_todos = getLocalStorage();
+  const [todos, setTodos] = useState(previous_todos ? previous_todos : []);
   const [inputValue, setInputValue] = useState("");
 
-  const handleAdd = (e) => {
-    setTodos([...todos, inputValue]);
-    setInputValue("");
+  const handleAdd = () => {
+    // add only if input is not empty
+    if (inputValue) {
+      // create a new copy of current todos
+      const new_todos = [...todos, inputValue];
+      // update todos state
+      setTodos(new_todos);
+      // update localStorage with new todos
+      updateLocalStorage(new_todos);
+      // clears input field
+      setInputValue("");
+    }
+  };
+
+  const handleKey = (e) => {
+    if (e.code === 'Enter') {
+      handleAdd();
+    }
   };
 
   const handleDelete = (index) => {
+    // create a new copy
     const new_todos = [...todos];
+    // remove item at index
     new_todos.splice(index, 1);
     setTodos(new_todos);
+    updateLocalStorage(new_todos);
   };
 
   return (
@@ -23,10 +53,13 @@ function App() {
           className="input-field"
           type="text"
           placeholder="Enter your todo item"
+          onKeyPress={handleKey}
           onChange={(e) => setInputValue(e.target.value)}
           value={inputValue}
         ></input>
-        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleAdd}>
+          Add
+        </button>
 
         <ul>
           {todos.map((item, index) => {
